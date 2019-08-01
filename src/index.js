29afe777afe63b0d063fff80d857f583a3286302
Simas <simonas.deimantavicius@mediapark.com>
2019-08-01 12:57:33 +0300

    setup redux structure and create store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import promiseMiddleware from 'redux-promise';
+
 import App from './components/App/App';
 import './index.css';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const storeWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+
+ReactDOM.render(<Provide store={storeWithMiddleware}><App /></Provide>, document.getElementById('root'));
