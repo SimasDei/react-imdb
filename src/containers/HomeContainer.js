4c983ffe7276d430ac9a0c8f480fddd672fa7311
Simas <simonas.deimantavicius@mediapark.com>
2019-08-02 15:28:54 +0300

    add home reducers

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -27,13 +27,6 @@ class Home extends Component {
     let endpoint = '';
     this.setState({ loading: true });
 
-    if (searchTerm === '') {
-      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage +
-        1}`;
-    } else {
-      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=${currentPage +
-        1}`;
-    }
     this.fetchItems(endpoint);
   };
 
--- src/reducers/home_reducer.js
+++ src/reducers/home_reducer.js
@@ -1,8 +1,60 @@
+import {
+  SHOW_LOADING_SPINNER,
+  SEARCH_MOVIES,
+  GET_POPULAR_MOVIES,
+  LOAD_MORE_MOVIES,
+  CLEAR_MOVIES,
+} from '../actions';
+
 const defaultState = {
   movies: [],
   heroImage: null,
   loading: false,
   currentPage: 0,
   totalPages: 0,
-  searchTerm: ''
+  searchTerm: '',
+};
+
+export default function(state = defaultState, action) {
+  switch (action.type) {
+    case GET_POPULAR_MOVIES:
+      return {
+        ...state,
+        movies: action.payload,
+        heroImage: state.heroImage || action.payload.results[0],
+        loading: false,
+        currentPage: action.payload.page,
+        totalPages: action.payload.total_pages,
+        searchTerm: '',
+      };
+    case LOAD_MORE_MOVIES:
+      return {
+        ...sate,
+        movies: [...sate.movies, ...action.payload.results],
+        currentPage: action.payload.page,
+        totalPages: action.payload.total_pages,
+        loading: false,
+      };
+    case SEARCH_MOVIES:
+      return {
+        ...state,
+        movies: action.payload.results,
+        loading: false,
+        currentPage: action.payload.page,
+        totalPages: action.payload.total_pages,
+        searchTerm: action.payload.searchTerm,
+      };
+    case CLEAR_MOVIES:
+      return {
+        ...state,
+        movies: [],
+      };
+    case SHOW_LOADING_SPINNER:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
 }
