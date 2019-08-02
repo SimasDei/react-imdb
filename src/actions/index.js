170489fa7c7a1952279577fa88cff9afc73baab0
Simas <simonas.deimantavicius@mediapark.com>
2019-08-02 15:00:18 +0300

    add more actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,13 @@ export const CLEAR_MOVIES = 'CLEAR_MOVIES';
 // actions  types for Both
 export const SHOW_LOADING_SPINNER = 'SHOW_LOADING_SPINNER';
 
+// Actions for Both
+export const showLoadingSpinner = () => ({
+  type: SHOW_LOADING_SPINNER,
+  payload: null,
+});
+
+// Actions for Home
 export const getPopularMovies = () => {
   const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
   const request = fetch(endpoint)
@@ -21,5 +28,55 @@ export const getPopularMovies = () => {
   return {
     type: GET_POPULAR_MOVIES,
     payload: request,
-  }
+  };
 };
+
+export const searchMovies = searchTerm => {
+  let endpoint;
+
+  if (!searchTerm) {
+    endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+  } else {
+    endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+  }
+
+  const request = fetch(endpoint)
+    .then(result => result.json())
+    .then(result => {
+      return { ...result, searchTerm };
+    })
+    .catch(error => console.error('Error:', error));
+
+  return {
+    type: SEARCH_MOVIES,
+    payload: request,
+  };
+};
+
+export const loadMoreMovies = (searchTerm, currentPage) => {
+  let endpoint;
+
+  if (searchTerm === '') {
+    endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage + 1}`;
+  } else {
+    endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=${currentPage +
+      1}`;
+  }
+
+  const request = fetch(endpoint)
+    .then(result => result.json())
+    .then(result => {
+      return { ...result, searchTerm };
+    })
+    .catch(error => console.error('Error:', error));
+
+  return {
+    type: LOAD_MORE_MOVIES,
+    payload: request,
+  };
+};
+
+export const clearMovies = () => ({
+  type: 'CLEAR_MOVIES',
+  payload: null,
+});
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,53 +1,46 @@
-
 class Home extends Component {
-
   componentDidMount() {
     if (sessionStorage.getItem('HomeState')) {
-      let state = JSON.parse(sessionStorage.getItem('HomeState'))
-      this.setState({ ...state })
+      let state = JSON.parse(sessionStorage.getItem('HomeState'));
+      this.setState({ ...state });
     } else {
-      this.setState({ loading: true })
+      this.setState({ loading: true });
       this.fetchItems(endpoint);
     }
   }
 
-  searchItems = (searchTerm) => {
+  searchItems = searchTerm => {
     let endpoint = '';
     this.setState({
       movies: [],
       loading: true,
-      searchTerm
-    })
+      searchTerm,
+    });
 
-    if (searchTerm === "") {
-      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-    } else {
-      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
-    }
     this.fetchItems(endpoint);
-  }
+  };
 
   loadMoreItems = () => {
     // ES6 Destructuring the state
     const { searchTerm, currentPage } = this.state;
 
     let endpoint = '';
-    this.setState({ loading: true })
+    this.setState({ loading: true });
 
     if (searchTerm === '') {
-      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage + 1}`;
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage +
+        1}`;
     } else {
-      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=${currentPage + 1}`;
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=${currentPage +
+        1}`;
     }
     this.fetchItems(endpoint);
-  }
+  };
 
-  fetchItems = (endpoint) => {
+  fetchItems = endpoint => {
     // ES6 Destructuring the state
     const { movies, heroImage, searchTerm } = this.state;
-
-    
-  }
+  };
 }
 
 export default Home;
